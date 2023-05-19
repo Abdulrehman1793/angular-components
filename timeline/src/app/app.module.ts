e5a7eb4741fbff4f9ca17b70cee199ac01314710
Abdulrehman1793 <abdulrehman1793@gmail.com>
2023-05-19 23:24:54 +0530

    Added animation to progress timeline

--- timeline/src/app/app.module.ts
+++ timeline/src/app/app.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProgressTimelineComponent } from './progress-timeline/progress-timeline.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProgressTimelineComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProgressTimelineComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- timeline/src/app/progress-timeline/progress-timeline.component.ts
+++ timeline/src/app/progress-timeline/progress-timeline.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 import { ITEMS } from './data';
 
+import {
+  trigger,
+  style,
+  animate,
+  transition,
+  query,
+  stagger,
+} from '@angular/animations';
+
+const listAnimation = trigger('listAnimation', [
+  transition('* <=> *', [
+    query(
+      ':enter',
+      [
+        style({ opacity: 0 }),
+        stagger('400ms ease-in', [
+          animate('50ms', style({ opacity: 0.25 })),
+          animate('10ms', style({ opacity: 0.4 })),
+          animate('150ms', style({ opacity: 0.6 })),
+          animate('200ms', style({ opacity: 1 })),
+        ]),
+      ],
+      { optional: true }
+    ),
+  ]),
+]);
+
 @Component({
   selector: 'app-progress-timeline',
   templateUrl: './progress-timeline.component.html',
   styleUrls: ['./progress-timeline.component.scss'],
+  animations: [listAnimation],
 })
 export class ProgressTimelineComponent {
   items = ITEMS;
