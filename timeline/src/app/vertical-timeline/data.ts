b173a0777c321eb1dbc7784fc8f16574f76e450c
Abdulrehman1793 <abdulrehman1793@gmail.com>
2023-05-20 00:49:55 +0530

    Successfully implemented vertical timeline

---
+++ timeline/src/app/vertical-timeline/data.ts
@@ -0,0 +1,44 @@
+export const ITEMS = [
+  {
+    title: 'Alphabet Inc.',
+    year: '2008 - 2010',
+    summary:
+      'The success of every website depend on search engine optimization and digital marketing strategy. If you are on first page of all major search...',
+    imageUrl: 'assets/images/vertical/google.png',
+  },
+  {
+    title: 'Amazon',
+    year: '2010 - 2012',
+    summary:
+      'The success of every website depend on search engine optimization and digital marketing strategy. If you are on first page of all major search...',
+    imageUrl: 'assets/images/vertical/amazon.png',
+  },
+  {
+    title: 'Flipkart',
+    year: '2021 - 2022',
+    summary:
+      'The success of every website depend on search engine optimization and digital marketing strategy. If you are on first page of all major search...',
+    imageUrl: 'assets/images/vertical/flipkart.png',
+  },
+  {
+    title: 'Tesla Corp.',
+    year: '2020 - 2021',
+    summary:
+      'The success of every website depend on search engine optimization and digital marketing strategy. If you are on first page of all major search...',
+    imageUrl: 'assets/images/vertical/tesla.png',
+  },
+  {
+    title: 'Toyota Motors.',
+    year: '2021 - 2022',
+    summary:
+      'The success of every website depend on search engine optimization and digital marketing strategy. If you are on first page of all major search...',
+    imageUrl: 'assets/images/vertical/toyota.png',
+  },
+  {
+    title: 'Youtube',
+    year: '2012',
+    summary:
+      'The success of every website depend on search engine optimization and digital marketing strategy. If you are on first page of all major search...',
+    imageUrl: 'assets/images/vertical/youtube.png',
+  },
+];
--- timeline/src/app/vertical-timeline/vertical-timeline.component.ts
+++ timeline/src/app/vertical-timeline/vertical-timeline.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ITEMS } from './data';
 
 @Component({
   selector: 'app-vertical-timeline',
   templateUrl: './vertical-timeline.component.html',
-  styleUrls: ['./vertical-timeline.component.scss']
+  styleUrls: ['./vertical-timeline.component.scss'],
 })
 export class VerticalTimelineComponent {
-
+  items = ITEMS;
 }
