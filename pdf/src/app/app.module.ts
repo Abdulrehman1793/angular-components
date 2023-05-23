d837643bde2a9943c1c699c9731c8df95577f49a
Abdulrehman1793 <abdulrehman1793@gmail.com>
2023-05-23 19:53:22 +0530

    implemented use of ngx-extended-pdf-viewer library

--- pdf/src/app/app.module.ts
+++ pdf/src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PdfViewerComponent } from './pdf-viewer/pdf-viewer.component';
 import { CustomPdfViewerComponent } from './custom-pdf-viewer/custom-pdf-viewer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PdfViewerComponent,
-    CustomPdfViewerComponent
-  ],
+  declarations: [AppComponent, PdfViewerComponent, CustomPdfViewerComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgxExtendedPdfViewerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- pdf/src/app/pdf-viewer/pdf-viewer.component.ts
+++ pdf/src/app/pdf-viewer/pdf-viewer.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pdf-viewer',
   templateUrl: './pdf-viewer.component.html',
-  styleUrls: ['./pdf-viewer.component.scss']
+  styleUrls: ['./pdf-viewer.component.scss'],
 })
 export class PdfViewerComponent {
+  selectedFile: any;
 
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    // Perform any necessary operations with the file
+    this.selectedFile = file;
+  }
 }
